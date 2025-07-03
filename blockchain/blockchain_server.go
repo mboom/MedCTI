14a3f67c495da164bc8d730c0d1de1c5779af89e
Marco Boom <m.boom@student.tudelft.nl>
2025-07-03 11:34:50 +0000

    added file operations

--- blockchain/blockchain_server.go
+++ blockchain/blockchain_server.go
@@ -2,19 +2,65 @@ package blockchain
 
 import (
 	"context"
+	"encoding/binary"
+	"os"
 	"google.golang.org/grpc"
-	empty "github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/mboom/MedCTI/blockchain"
 )
+
+const ledger = "/var/tmp/blockchain-ledger.binpb"
 
 type blockchainServer struct {
 	pb.UnimplementedBlockchainServer
 }
 
-func (bs *blockchainServer) PublishLogData(context.Context, flow *pb.Flow) (*empty.Empty, error) {
-	return nil
+func (bs *blockchainServer) PublishLogData(_ context.Context, flow *pb.Flow) (*empty.Empty, error) {
+	f, err := os.OpenFile(ledger, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	data, err := proto.Marshal(flow)
+	if err != nil {
+		panic(err)
+	}
+	size := make([]byte, 4)
+	_, err = binary.Encode(size, binary.BigEndian, uint32(len(data)))
+	if err != nil {
+		panic(err)
+	}
+	data = size + data
+
+	if _, err = f.Write(data); err != nil {
+		panic(err)
+	}
+
+	return empty.Empty{}, nil
 }
 
-func (bs *blockchainServer) FetchLogData(*empty.Empty, stream Blockchain_FetchLogDataClient) error {
+func (bs *blockchainServer) FetchLogData(_ *empty.Empty, stream Blockchain_FetchLogDataClient) error {
+	f, err := os.OpenFile(ledger, os.O_RDONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	sizebuffer := make([]byte, 4)
+	_, err = f.Read(sizebuffer)
+	if err != nil {
+		panic(err)
+	}
+	size := binary.BigEndian.Uint32(sizebuffer)
+	data := make([]byte, size)
+	_, err = f.Read(data)
+	if err != nil {
+		panic(err)
+	}
+
+	flow := &pb.Flow{}
+	err = proto.Unmarshal(data, flow)
 	return nil
 }
