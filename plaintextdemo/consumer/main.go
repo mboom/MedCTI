c21b1a101d8ad09dea6c5424bc04e8ead8a4a023
Marco Boom <m.boom@student.tudelft.nl>
2025-07-04 12:23:35 +0000

    added generated module files

--- plaintextdemo/consumer/main.go
+++ plaintextdemo/consumer/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	blockchain "github.com/mboom/MedCTI/blockchain/proto"
+)
 
 func main() {
 	name := "MedCTI"
 	fmt.Println("Project for", name)
+	fmt.Println(TapTraffic())
+}
+
+func TapTraffic() blockchain.Flow {
+	flow := blockchain.Flow{id: uuid.New(), kid: uuid.New(), destination: [1]byte{2}, source: [1]byte{1} }
+	return flow
 }
