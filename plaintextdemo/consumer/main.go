db3b2cc102e686f62e344c64cdbdb44c0be0ede3
Marco Boom <m.boom@student.tudelft.nl>
2025-07-08 13:37:54 +0000

    Split consumer and provider

--- plaintextdemo/consumer/main.go
+++ plaintextdemo/consumer/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"github.com/golang/protobuf/ptypes/empty"
 	blockchain "github.com/mboom/MedCTI/blockchain/proto"
 )
 
@@ -17,45 +16,36 @@ var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
-func main() {
-	// parse command line arguments
-	flag.Parse()
-
-	// create connection to the blockchain simulator
+// create connection to the blockchain simulator
+func connect() (*grpc.ClientConn, blockchain.BlockchainClient) {
 	conn, err := grpc.NewClient(fmt.Sprintf("%v:%d", *host, *port), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close()
 	ledger := blockchain.NewBlockchainClient(conn)
+	return conn, ledger
+}
 
-	// publish a message
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	_, err = ledger.PublishLogData(ctx, TapTraffic())
+// publish a flow
+func publish(ctx context.Context, ledger blockchain.BlockchainClient) {
+	_, err := ledger.PublishLogData(ctx, &blockchain.Flow{Id: uuid.New().ID(), Kid: uuid.New().ID(), Destination: []byte{2}, Source: []byte{1}})
 	if err != nil {
 		panic(err)
-	}
-
-	// create blockchain stream reader
-	data, err := ledger.FetchLogData(ctx, &empty.Empty{})
-	if err != nil {
-		panic(err)
-	}
-
-	// receive data from blockchain
-	for err == nil {
-		flow, err := data.Recv()
-		if err != nil {
-			break
-		}
-
-		// print data
-		fmt.Println(flow)
 	}
 }
 
-func TapTraffic() *blockchain.Flow {
-	flow := blockchain.Flow{Id: uuid.New().ID(), Kid: uuid.New().ID(), Destination: []byte{2}, Source: []byte{1} }
-	return &flow
+func main() {
+	// parse command line arguments
+	flag.Parse()
+
+	// create connection
+	conn, ledger := connect()
+	defer conn.Close()
+
+	// create context
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// publish a message
+	publish(ctx, ledger)
 }
--- plaintextdemo/provider/main.go
+++ plaintextdemo/provider/main.go
@@ -1,8 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"github.com/golang/protobuf/ptypes/empty"
+	blockchain "github.com/mboom/MedCTI/blockchain/proto"
+)
+
+var (
+	host = flag.String("host", "localhost", "The hostname or IP address that will be used to listen.")
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// create connection to the blockchain simulator
+func connect() (*grpc.ClientConn, blockchain.BlockchainClient) {
+	conn, err := grpc.NewClient(fmt.Sprintf("%v:%d", *host, *port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	ledger := blockchain.NewBlockchainClient(conn)
+	return conn, ledger
+}
+
+func read(ctx context.Context, ledger blockchain.BlockchainClient) {
+	// create blockchain stream reader
+	data, err := ledger.FetchLogData(ctx, &empty.Empty{})
+	if err != nil {
+		panic(err)
+	}
+
+	// receive data from blockchain
+	for err == nil {
+		flow, err := data.Recv()
+		if err != nil {
+			break
+		}
+
+		// print data
+		fmt.Println(flow)
+	}
+}
 
 func main() {
-	name := "MedCTI"
-	fmt.Println("Project for", name)
+	// parse command line arguments
+	flag.Parse()
+
+	// create connection
+	conn, ledger := connect()
+	defer conn.Close()
+
+	// create context
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// read blockchain
+	read(ctx, ledger)
 }
